a1240f4b6f273fcf2a87925741e07ff99610a468
InzamamRahaman <izzyrahaman@gmail.com>
2015-08-13 21:18:40 -0400

    Added code to read FASTA files - code needs further testing

--- FSharpBio/FastaReader.fs
+++ FSharpBio/FastaReader.fs
@@ -5,3 +5,20 @@ module FastaReader =
     open FSharpBio.Nucleotide
     open FSharpBio.FastaRecord
 
+    let isNotMarker (s : string) = 
+        s.StartsWith(">") = false
+
+    let rec splitByFastaMarker (xs : seq<string>) = 
+        seq {
+            let nameOfSequence = Seq.take 1 xs
+            let ys = Seq.skip 1 xs
+            let sequence = Seq.takeWhile isNotMarker ys
+            let zs = Seq.skipWhile isNotMarker ys
+            yield {sequence = stringToNucleotideSequence (Seq.reduce (+)sequence); 
+                name = Seq.reduce (+) nameOfSequence}
+            yield! splitByFastaMarker zs
+        }
+
+    let read filepath = 
+        let lines = System.IO.File.ReadLines(filepath)
+        lines |> splitByFastaMarker
